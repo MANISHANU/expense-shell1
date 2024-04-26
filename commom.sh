667544a63a7f14d66caf172c5cc47d3c5eb46fd0
Manisha <manishamaidam169@gmail.com.com>
2024-04-26 23:22:07 +0530

    commom

---
+++ commom.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#!/bin/bash
+
+set -e 
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+check-user()
+{
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root access."
+    exit 1 # manually exit if error comes.
+else
+    echo "You are super user."
+fi
+
+}
